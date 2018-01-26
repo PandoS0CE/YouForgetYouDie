9c6c6ebeb4aaebd2a9cdcaf62353a60436f0ea75
Pando <paulovern@gmail.com>
2018-01-26 23:04:01 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private Rigidbody2D rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveHorizontal, 0) * speed;
+        
+	}
+}
