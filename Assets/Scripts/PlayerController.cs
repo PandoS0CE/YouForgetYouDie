f8ef9b959d2b03ce400d111a8452cd3c2a0bdfb3
Pando <paulovern@gmail.com>
2018-01-26 23:43:50 +0100

    Ajout Saut

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-
+    private bool jumpEnable = false;
     private Rigidbody2D rb;
     public float speed;
+    public float jumpPower;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,22 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(moveHorizontal, 0) * speed;
-        
+        Saut();
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        jumpEnable = collision.collider.gameObject.CompareTag("Ground");
+
+   
+    }
+    void Saut()
+    {
+        if (Input.GetKeyDown("space") && jumpEnable == true)
+        {
+            rb.AddForce(new Vector2(0,1) * jumpPower, ForceMode2D.Impulse);
+            jumpEnable = false;
+        }
+    }
 }
