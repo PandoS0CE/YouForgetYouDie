67732b3d76918d0a212d1be8cf40e18ea168d2c4
Nassafy <matthiaspinard@aol.com>
2018-01-28 15:43:47 +0100

    Galette saucisse fonctionnel

---
+++ Assets/Scripts/VacheControllers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacheControllers : IA_Ennemy {
+
+    public int pdv;
+
+    new void  OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("GaletteSaucisse"))
+        {
+            pdv--;
+            if(pdv == 0)
+            {
+                DestroyObject(gameObject);
+            }
+        }
+    }
+}
